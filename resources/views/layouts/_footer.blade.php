d367c42e6839378f9cb2f8c676cb976580279a97
JohnLu <johnluchn@gmail.com>
2018-02-13 19:21:38 +0800

    局部视图与链接优化

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,13 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34" alt="">
+            <a href="">千锤百炼，方能成钢</a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{ route('help') }}">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
