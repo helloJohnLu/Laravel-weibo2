a4768f23fc158159303f99fe243ebd18bc6f17be
JohnLu <johnluchn@gmail.com>
2018-02-15 01:02:09 +0800

    友好的跳转，登录后重定向到用户上一次请求的页面

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -33,7 +33,7 @@ class SessionsController extends Controller
         /* 逻辑 */
         if (\Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', '欢迎回来！');
-            return redirect()->route('users.show',[\Auth::user()]);
+            return redirect()->intended(route('users.show',[\Auth::user()]));
         }else{
             /* 消息提示和页面重定向 */
             session()->flash('danger','很抱歉，您的邮箱与密码不匹配');
