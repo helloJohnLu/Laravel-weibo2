4cf7d97d44c5d4df210fe36cd6f0cbe6fd473c8e
JohnLu <johnluchn@gmail.com>
2018-02-14 12:00:28 +0800

    Finish user signup,用户注册完成

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,6 +13,30 @@ class UsersController extends Controller
         return view('users.create');
     }
 
+    /* 处理用户注册表单提交 */
+    public function store(Request $request)
+    {
+        /* 校验数据合法性 */
+        $this->validate($request,[
+            'name'      =>  'required|max:50|min:3',
+            'email'     =>  'required|email|unique:users|max:255',
+            'password'  =>  'required|confirmed|min:6'
+        ]);
+
+        /* 逻辑 */
+        $user = User::create([
+            'name'      =>  $request->name,
+            'email'     =>  $request->email,
+            'password'  =>  bcrypt($request->password),
+        ]);
+
+        /* 显示注册成功的提示信息 */
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+
+        return redirect()->route('users.show',[$user]);
+    }
+
+
     // 显示用户信息
     public function show(User $user)
     {
