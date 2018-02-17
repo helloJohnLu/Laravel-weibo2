2f1df441d80c94c24c7f11a7fb8198d7ddcd15af
JohnLu <johnluchn@gmail.com>
2018-02-17 08:42:24 +0800

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Status;
 use Illuminate\Http\Request;
 
 class StatusesController extends Controller
@@ -22,6 +23,15 @@ class StatusesController extends Controller
             'content'   =>  \request('content')
         ]);
 
+        return redirect()->back();
+    }
+
+    // 删除微博
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success','微博已被成功删除！');
         return redirect()->back();
     }
 }
